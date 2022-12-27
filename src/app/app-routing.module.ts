47809afd90a3ef2e1dbbd5aa8eedca3e2e100a64
manuelrojas <manuel.rojas.21@gmail.com>
2022-12-27 08:36:10 -0600

    page not found img

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NewsItemComponent } from './news-item/news-item.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/news', pathMatch: 'full' },
   { path: 'news/:id', component: NewsItemComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { DotCmsService } from './shared/dotcms.service';
 import { MenuComponent } from './menu/menu.component';
 import { TimeAgoPipe } from './shared/pipe/time-ago.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, TimeAgoPipe],
+  declarations: [AppComponent, MenuComponent, TimeAgoPipe, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [DotCmsService],
   bootstrap: [AppComponent],
